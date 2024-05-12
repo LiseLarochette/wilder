47ad32c9c6409dccd8677bce1c7d2ad814033222
Lise LAROCHETTE <168559876+LiseLarochette@users.noreply.github.com>
2024-05-12 19:29:08 +0200

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-st.page_link("your_app.py", label="Home", icon="🏠")
+st.page_link("my_streamlit_app.py", label="Home", icon="🏠")
 st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
 st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
 st.page_link("http://www.google.com", label="Google", icon="🌎")
